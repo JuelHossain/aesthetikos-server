676a43765592617d36fb478e356250a8f1cca04f
juelhossain <juelhossainnn@gmail.com>
2022-11-25 22:37:42 +0600

    modified getproducts api

--- controller/productsController/getProducts.js
+++ controller/productsController/getProducts.js
@@ -5,8 +5,10 @@ const getProducts = async (req, res) => {
   try {
     const page = parseInt(req.query.page, 10);
     const size = parseInt(req.query.size, 10);
-    const { cat } = req.query;
-    const query = cat ? { cat } : {};
+    const { cat, email } = req.query;
+    const query = {};
+    if (cat) Object.assign(query, { cat });
+    if (email) Object.assign(query, { createdBy: email });
 
     const cursor = productsCollection.find(query).sort({ createdAt: -1 });
 
