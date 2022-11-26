bf4a22151d3f966b9506626a92fbcd8ef1488480
juelhossain <juelhossainnn@gmail.com>
2022-11-26 13:07:25 +0600

    getusers api updated

--- controller/usersController/getUsers.js
+++ controller/usersController/getUsers.js
@@ -5,8 +5,11 @@ const getUsers = async (req, res) => {
   try {
     const page = parseInt(req.query.page, 10);
     const size = parseInt(req.query.size, 10);
-    const { role } = req.query;
-    const query = role ? { role } : {};
+    const { seller, admin, buyer } = req.query;
+    const query = {};
+    if (admin) Object.assign(query, { admin: true });
+    if (seller) Object.assign(query, { seller: true });
+    if (buyer) Object.assign(query, { seller: false, admin: false });
     const cursor = usersCollection.find(query);
 
     let users;
